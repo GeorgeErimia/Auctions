aaae0bfa3d9b152600bce829a0a096d3058794bb
George Erimia <l3ttuceftw@gmil.com>
2024-05-16 09:14:35 +0300

    Added Missing Files

--- src/main/frontend/auctions_frontend/src/components/FooterComponent.jsx
+++ src/main/frontend/auctions_frontend/src/components/FooterComponent.jsx
@@ -2,23 +2,20 @@ import React from "react";
 
 const FooterComponent = () => {
   return (
-    <div>
-      <footer
-        className="footer"
-        style={{
-          position: "absolute",
-          bottom: "0",
-          width: "100%",
-          maxWidth: "900px",
-          height: "60px",
-          backgroundColor: "black",
-          color: "white",
-          textAlign: "center",
-        }}
-      >
-        <p className="text-center">Auctions App By George Erimia</p>
-      </footer>
-    </div>
+    <footer
+      className="footer footer-component"
+      style={{
+        position: "absolute",
+        bottom: "0",
+        width: "100%",
+        height: "60px",
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <p className="text-center">Auctions App By George Erimia</p>
+    </footer>
   );
 };
 
